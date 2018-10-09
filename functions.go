dc4b526ea2535d440244a753181e89351eeaaa6a
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-10 07:06:21 +0900

    Multiple results

--- functions.go
+++ functions.go
@@ -6,6 +6,12 @@ func add(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
