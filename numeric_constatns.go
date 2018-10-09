f18501291f1c6ba01ffa7e9cd61b3327c21e935f
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-10 07:55:37 +0900

    needInt(Big)を出力

--- numeric_constatns.go
+++ numeric_constatns.go
@@ -11,6 +11,10 @@ func needInt(x int) int           { return x*10 + 1 }
 func needFloat(x float64) float64 { return x * 0.1 }
 
 func main() {
+	// fmt.Println(Big)
+	// fmt.Println(Small)
+	// fmt.Println(needInt(Big))
+
 	fmt.Println(needInt(Small))
 	fmt.Println(needFloat(Small))
 	fmt.Println(needFloat(Big))
