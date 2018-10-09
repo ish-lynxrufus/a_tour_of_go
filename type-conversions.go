475ca9bf1ba74460d978ceed713e019d8c05b69f
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-10 07:35:33 +0900

    型変換の削除

--- type-conversions.go
+++ type-conversions.go
@@ -9,5 +9,7 @@ func main() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*y + y*y))
 	var z uint = uint(f)
+	// var f float64 = x*y + y*y
+	// var z uint = f
 	fmt.Println(x, y, z)
 }
