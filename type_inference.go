80d67af235178d751470965d1552782b12b0870f
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-10 07:42:04 +0900

    型変化の確認

--- type_inference.go
+++ type_inference.go
@@ -5,4 +5,14 @@ import "fmt"
 func main() {
 	v := 42
 	fmt.Printf("v is of type %T\n", v)
+
+	i := 42
+	f := 3.142
+	g := 0.867 + 0.5i
+	s := "hoge"
+
+	fmt.Printf("i is of type %T\n", i)
+	fmt.Printf("f is of type %T\n", f)
+	fmt.Printf("g is of type %T\n", g)
+	fmt.Printf("s is of type %T\n", s)
 }
