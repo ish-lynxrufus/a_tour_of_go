315b703192203dfa826b85b61918da80dc3aa057
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-10 18:00:27 +0900

    スライスの確認-4

--- append.go
+++ append.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func main() {
 	var s []int
+	printSlice(s) // 0
+
+	s = append(s, 0) // 1
 	printSlice(s)
 
-	s = append(s, 0)
+	s = append(s, 1, 2) // 4
 	printSlice(s)
 
-	s = append(s, 1)
-	printSlice(s)
-
-	s = append(s, 2, 3, 4)
+	s = append(s, 2, 3, 4) // 8
 	printSlice(s)
 }
 
