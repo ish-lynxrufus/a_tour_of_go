91934678c71979148ac01cb10ea843c6d51da98e
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-11 07:12:30 +0900

    Pointers and functions

--- methods_pointers.go
+++ methods_pointers.go
@@ -9,21 +9,17 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v Vertex) Scale(f float64) Vertex {
+func Scale(v *Vertex, f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
-	return v
-
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(v)
-	x := v.Scale(10)
-	fmt.Println(x)
-	fmt.Println(v.Abs())
+	Scale(&v, 10)
+	fmt.Println(Abs(v))
 }
