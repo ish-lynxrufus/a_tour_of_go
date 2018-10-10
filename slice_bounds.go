36044313e35acdce42583f3ec36b0fafd7e2fb31
ish-lynxrufus <20295083+ish-lynxrufus@users.noreply.github.com>
2018-10-10 16:57:50 +0900

    スライスの確認

--- slice_bounds.go
+++ slice_bounds.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
 
+	fmt.Println(s[1:4]) // 3, 5, 7
+	fmt.Println(s[:2])  // 2, 3
+	fmt.Println(s[1:])  // 3, 5, 7, 11, 13
+
 	s = s[1:4]
 	fmt.Println(s) // 3, 5, 7
 
